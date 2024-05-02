370538f08c9ca2fb05e030b28503993c6a04bf31
akoreli <akorelii.00@gmail.com>
2024-05-02 09:12:15 +0300

    Add project files.

---
+++ Stars/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Enter Edge Number: ");
+        int square = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 0; i < square; i++)
+        {
+            for (int j = 0; j < square; j++)
+            {
+                if (i == 0 || i == square - 1 || j == 0 || j == square - 1)
+                {
+                    Console.Write("* ");
+                }
+                else
+                {
+                    Console.Write("  ");
+                }
+            }
+            Console.WriteLine();
+        }
+
+        Console.Read();
+    }
+}
